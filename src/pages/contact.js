58948aa138149f04bc3f17722427a80730ef5c55
gitMarcldo <marcldo@users.noreply.github.com>
2021-04-19 20:54:50 -0400

    contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>marc@mail.con</p>
+    </div>
+  );
+};
+
+export default ContactPage;
