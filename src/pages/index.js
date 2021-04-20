c1d44b6a8231f974a6db4039903ee5bbeadf5e5d
gitMarcldo <marcldo@users.noreply.github.com>
2021-04-19 20:52:23 -0400

    index about and blog pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello,</h1>
+      <h2>I'm Marc, a full-stack developer</h2>
+    </div>
+  );
+};
+
+export default IndexPage;
+
