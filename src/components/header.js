64dffc993f642c85899b0d28deadf07ac3ea5f0e
gitMarcldo <marcldo@users.noreply.github.com>
2021-04-23 22:03:10 -0400

    header and layout scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import * as headerStyles from './header.module.scss';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Great Gatsby Blog</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Great Gatsby
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
