8ac8f5e8e8e610098fa4ba372cf71edb074f5800
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-03 19:21:29 -0400

    installed transform-remark and imported blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node{
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+  `);
+  const posts = data.allMarkdownRemark.edges
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on</p>
+      <ol>
+        {posts.map((post) => (
+          <li key={post.node.frontmatter.title}>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   );
 };
