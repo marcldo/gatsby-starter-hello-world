04c803acdf13da6cc954ab087c360b4ba068e02b
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-11 11:56:52 -0400

    contentful rich-text images rendered in react

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from "@contentful/rich-text-types";
 
 import Layout from '../components/layout';
 
@@ -11,19 +12,42 @@ export const query = graphql`
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
         raw
+        references {
+          ... on ContentfulAsset {
+             contentful_id
+             title
+             file {
+                url
+             }
+          }
+       }
       }
     }
   }
 `;
 
 const Blog = (props) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const imageID = node.data.target.sys.id;
+        const {
+          file: { url },
+          title
+        } = props.data.contentfulBlogPost.body.references.find(({ contentful_id: id }) => id === imageID);
+        return <img alt={title} src={url} />
+      }
+    }
+  };
+
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
       {documentToReactComponents(
-        JSON.parse(props.data.contentfulBlogPost.body.raw)
-      )}
+        JSON.parse(props.data.contentfulBlogPost.body.raw), options
+      )
+      }
     </Layout>
   );
 };
