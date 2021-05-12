99dd599fd3c068b0714a0a8b8f17287d5328243d
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-12 12:18:33 -0400

    netlify test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello,</h1>
       <h2>I'm Marc, a full-stack developer</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+      <p>Need a developer?</p>
     </Layout>
   );
 };
