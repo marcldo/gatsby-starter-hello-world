54a2fce5844aea45133500d0c6d7a16b02761e68
gitMarcldo <marcldo@users.noreply.github.com>
2021-05-31 10:33:05 -0400

    updated title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: 'Full-Stack Bootcamp',
+    title: 'Gatsby Template',
     author: 'Marc Felizardo'
   },
   plugins: [
